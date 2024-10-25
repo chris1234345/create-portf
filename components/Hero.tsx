cbef8df0e312deaf02f68a3bcea1437de1e80d66
Chris TF <81719250+chris1234345@users.noreply.github.com>
2024-10-25 10:53:09 +0300

    commit

---
+++ components/Hero.tsx
@@ -0,0 +1,34 @@
+import { Spotlight } from '@/lib/ui/Sportlight'
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <div className="pb-20 pt-36">
+    {/**
+     *  UI: Spotlights
+     *  Link: https://ui.aceternity.com/components/spotlight
+     */}
+    <div>
+      <Spotlight
+        className="-top-40 -left-10 md:-left-32 md:-top-20 h-screen"
+        fill="white"
+      />
+      <Spotlight
+        className="h-[80vh] w-[50vw] top-10 left-full"
+        fill="purple"
+      />
+      <Spotlight className="left-80 top-28 h-[80vh] w-[50vw]" fill="blue" />
+    </div>
+
+    <div className="h-[50rem] w-full dark:bg-black bg-white  dark:bg-grid-white/[0.2] bg-grid-black/[0.2] relative flex items-center justify-center">
+      {/* Radial gradient for the container to give a faded look */}
+      <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
+      <p className="text-4xl sm:text-7xl font-bold relative z-20 bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-500 py-8">
+        Backgrounds
+      </p>
+    </div>
+    </div>
+  )
+}
+
+export default Hero
